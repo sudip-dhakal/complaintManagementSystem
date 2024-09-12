abedaf64c0dab6559a3cb9ed699344cda4d7c67c
sudip-dhakal <dhakalsudip900@gmail.com>
2024-09-12 15:35:14 +0545

    responsiveness

--- src/Components/Entrance_Page/SignUp_Page/Signup.js
+++ src/Components/Entrance_Page/SignUp_Page/Signup.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signup.css";
 import Header from "../../Header/Header";
 import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [user_name, setUser_name] = useState("");
+  const [myemail, setMyemail] = useState("");
+  const [psw, setPsw] = useState("");
+
+  let sendSignUpData = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://66d581f5f5859a704266544c.mockapi.io/complainSys/data", {
+        username: user_name,
+        email: myemail,
+        password: psw,
+        complaint: "",
+        subject: "",
+      })
+      .then(() => {
+        alert("sign up successful");
+      });
+  };
+
   let Navigate = useNavigate();
   return (
     <React.Fragment>
@@ -12,19 +34,39 @@ const Signup = () => {
 
       <form className="signup_form">
         <div className="name">
-          <input type="text" placeholder="Your Name" />
+          <input
+            type="text"
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="username">
-          <input type="username" placeholder="Your username" />
+          <input
+            type="username"
+            placeholder="Your username"
+            value={user_name}
+            onChange={(e) => setUser_name(e.target.value)}
+          />
         </div>
         <div className="email">
-          <input type="email" placeholder="Your email" />
+          <input
+            type="email"
+            placeholder="Your email"
+            value={myemail}
+            onChange={(e) => setMyemail(e.target.value)}
+          />
         </div>
         <div className="password">
-          <input type="password" placeholder="Your password" />
+          <input
+            type="password"
+            placeholder="Your password"
+            value={psw}
+            onChange={(e) => setPsw(e.target.value)}
+          />
         </div>
 
-        <button id="signup-submit" type="submit">
+        <button id="signup-submit" type="submit" onClick={sendSignUpData}>
           Sign up
         </button>
         <p id="login">
